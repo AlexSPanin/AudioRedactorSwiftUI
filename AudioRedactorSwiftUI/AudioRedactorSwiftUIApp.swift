c73b6deacb52f05f687379968b78acac8ae35771
Alexander Panin <panin1970@gmail.com>
2022-03-14 13:35:07 +0300

    Initial Commit

---
+++ AudioRedactorSwiftUI/AudioRedactorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioRedactorSwiftUIApp.swift
+//  AudioRedactorSwiftUI
+//
+//  Created by Александр Панин on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AudioRedactorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
